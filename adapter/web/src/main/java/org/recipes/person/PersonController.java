257761445de53a433ba3d40afa9a62ecd05096ad
Johanna.Rasu <johanna.rasu@nortal.com>
2023-06-22 11:24:14 +0300

    Liquibase and lombok exercises

--- adapter/web/src/main/java/org/recipes/person/PersonController.java
+++ adapter/web/src/main/java/org/recipes/person/PersonController.java
@@ -2,7 +2,7 @@ package org.recipes.person;
 
 
 //TODO:
-// 1. Write a POST method to take in PersonDto class with or without an id
+// 1. Write a POST method to take in PersonDto class with and without an id
 // 2. Call SavePersonUseCase with Person class
 // 3. Convert back to PersonDto and retrieve it with id
 public class PersonController {
---
+++ app-domain/src/main/java/org/recipes/model/person/User.java
@@ -0,0 +1,80 @@
+package org.recipes.model.person;
+
+import java.util.Objects;
+
+public class User {
+    private String userName;
+    private String personId;
+    private Role role;
+
+    public User(String userName, String personId, Role role) {
+        this.userName = userName;
+        this.personId = personId;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userName, user.userName) && Objects.equals(personId,
+                user.personId) && role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, personId, role);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", personId='" + personId + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    public enum Role {
+        CLIENT,
+        OFFICIAL,
+        EXTERNAL_OFFICIAL
+    }
+
+    public static void main(String[] args) {
+
+        User officer = new User("officer", "123", Role.OFFICIAL);
+        officer.setUserName(officer.getUserName() + "2");
+        officer.setPersonId( "0");
+        officer.setRole(Role.CLIENT);
+
+        System.out.println("Username: " + officer.getUserName() + ", personId: " + officer.getPersonId() + ", " +
+                "role: " + officer.getRole());
+        System.out.println(officer);
+    }
+}
